75b50123745a62ef34ad382ff87ce41df986d5f6
markus.klein <klma1021(at)hs-karlsruhe.de>
2013-04-22 11:12:42 +0200

    Added updateAdresse() to AdresseResourceTest.java

--- shop/src/test/java/de/shop/kundenverwaltung/rest/AdresseResourceTest.java
+++ shop/src/test/java/de/shop/kundenverwaltung/rest/AdresseResourceTest.java
@@ -2,16 +2,16 @@ package de.shop.kundenverwaltung.rest;
 
 import java.io.StringReader;
 import java.lang.invoke.MethodHandles;
+import java.util.Set;
 
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
-import javax.json.JsonValue;
 
 import static de.shop.util.TestConstants.ADRESSEN_ID_PATH;
 import static de.shop.util.TestConstants.ADRESSEN_ID_PATH_PARAM;
 import static de.shop.util.TestConstants.ADRESSEN_PATH;
 import static de.shop.util.TestConstants.LOCATION;
-import static de.shop.util.TestConstants.ZAHLUNGSINFORMATIONEN_PATH;
 
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.logging.Logger;
@@ -23,13 +23,13 @@ import com.jayway.restassured.response.Response;
 
 import static com.jayway.restassured.RestAssured.given;
 import static java.net.HttpURLConnection.HTTP_CREATED;
+import static java.net.HttpURLConnection.HTTP_NO_CONTENT;
 import static java.net.HttpURLConnection.HTTP_OK;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.runners.MethodSorters.NAME_ASCENDING;
 import static de.shop.util.TestConstants.ACCEPT;
-import de.shop.kundenverwaltung.domain.AdresseLandType;
 import de.shop.util.AbstractResourceTest;
 
 @RunWith(Arquillian.class)
@@ -44,6 +44,8 @@ public class AdresseResourceTest extends AbstractResourceTest {
 	private static final String PLZ_NEU = "76133";
 	private static final String ORT_NEU = "Karlsruhe";
 	private static final String LAND_NEU = "DE";
+	
+	private static final String STRASSE_UPDATE = "Moltkestraße 333";
 
 	@Test
 	public void validate() {
@@ -107,5 +109,45 @@ public class AdresseResourceTest extends AbstractResourceTest {
 		assertThat(id.longValue() > 0, is(true));
 
 		LOGGER.debugf("ENDE createAdresse");
+	}
+	
+	@Test
+	public void updateAdresse() {
+		final Long adresseId = ADRESSE_ID_VORHANDEN;
+		final String strasseUpdate = STRASSE_UPDATE;
+		
+		//When
+		Response response = given().header(ACCEPT, APPLICATION_JSON)
+										 .pathParam(ADRESSEN_ID_PATH_PARAM, adresseId)
+										 .get(ADRESSEN_ID_PATH);
+		
+		JsonObject jsonObject;
+		try (final JsonReader jsonReader =
+				 getJsonReaderFactory().createReader(new StringReader(response.asString()))) {
+			jsonObject = jsonReader.readObject();
+		}
+    	assertThat(jsonObject.getJsonNumber("adresseId").longValue(), is(adresseId.longValue()));
+		
+    	// Aus den gelesenen JSON-Werten ein neues JSON-Objekt mit neuem Nachnamen bauen
+    	final JsonObjectBuilder job = getJsonBuilderFactory().createObjectBuilder();
+    	final Set<String> keys = jsonObject.keySet();
+    	for (String k : keys) {
+    		if ("strasse".equals(k)) {
+    			job.add("strasse", strasseUpdate);
+    		}
+    		else {
+    			job.add(k, jsonObject.get(k));
+    		}
+    	}
+    	jsonObject = job.build();
+    	
+		response = given().contentType(APPLICATION_JSON)
+				          .body(jsonObject.toString())
+                          //.auth()
+                          //.basic(username, password)
+                          .put(ADRESSEN_PATH);
+		
+		// Then
+		assertThat(response.getStatusCode(), is(HTTP_NO_CONTENT));
 	}
 }
