d5b9aa8220fc3fd7e822cdb679b6b1b172f5c6c5
thomas.beuz <beth1015(at)hs-karlsruhe.de>
2013-04-23 13:39:51 +0200

    tests für kategorie & artikel

--- shop/src/test/java/de/shop/artikelverwaltung/rest/ArtikelResourceTest.java
+++ shop/src/test/java/de/shop/artikelverwaltung/rest/ArtikelResourceTest.java
@@ -43,6 +43,7 @@ import static de.shop.util.TestConstants.ARTIKEL_NAME_QUERY_PARAM;
 import static de.shop.util.TestConstants.ARTIKEL_PATH;
 import static de.shop.util.TestConstants.KATEGORIE_ID_PATH;
 import static de.shop.util.TestConstants.KATEGORIE_ID_PATH_PARAM;
+import static de.shop.util.TestConstants.KATEGORIE_PATH;
 import static de.shop.util.TestConstants.LOCATION;
 import static de.shop.util.TestConstants.PORT;
 import static java.net.HttpURLConnection.HTTP_CONFLICT;
@@ -57,6 +58,7 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static org.hamcrest.CoreMatchers.anyOf;
 import static org.hamcrest.CoreMatchers.endsWith;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
@@ -76,6 +78,9 @@ import java.util.Set;
 
 
 
+
+
+
 import javax.json.JsonArray;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -92,7 +97,13 @@ import com.jayway.restassured.response.Response;
 
 
 
+
+
+
 import de.shop.util.AbstractResourceTest;
+import de.shop.artikelverwaltung.domain.Kategorie;
+import de.shop.artikelverwaltung.rest.KategorieResource;
+import de.shop.artikelverwaltung.rest.ArtikelResource;
 
 
 @RunWith(Arquillian.class)
@@ -101,16 +112,24 @@ public class ArtikelResourceTest extends AbstractResourceTest {
 	private static final Logger LOGGER = Logger.getLogger(MethodHandles.lookup().lookupClass().getName());
 	
 	private static final Long ARTIKEL_ID_VORHANDEN = Long.valueOf(200);
+	private static final Long ARTIKEL_ID_UPDATE = Long.valueOf(210);
 	private static final Long ARTIKEL_ID_NICHTVORHANDEN = Long.valueOf(20000);
+	private static final String NEUE_BEZEICHNUNG = "wundervolle graue Damenhose gangnamstyle";
+	private static final String UPDATE_BEZEICHNUNG = "wundervoller schwarzer Langmantel";
+	private static final String NEUER_NAME = "HOSE";
+	private static final Long NEUE_KATEGORIE_ID = Long.valueOf(500);
+	private static final Float NEUER_PREIS = Float.valueOf((float) 1000.00);
 	
 	
 	@Test
 	public void validate() {
+
 		assertThat(true, is(true));
 	}
+	
 	@Test
 	public void findArtikelById() {
-		LOGGER.debugf("BEGINN");
+		LOGGER.debugf("BEGINN ARTIKELSUCHE 1");
 		
 		// Given
 		final Long artikelId = ARTIKEL_ID_VORHANDEN;
@@ -129,12 +148,12 @@ public class ArtikelResourceTest extends AbstractResourceTest {
 			assertThat(jsonObject.getJsonNumber("artikelId").longValue(), is(artikelId.longValue()));
 		}
 		
-		LOGGER.debugf("ENDE");
+		LOGGER.debugf("ENDE ARTIKELSUCHE 1");
 	}
 	
 	@Test
 	public void findArtikelByIdNichtVorhanden() {
-		LOGGER.debugf("BEGINN");
+		LOGGER.debugf("BEGINN ARTIKELSUCHE 2");
 		
 		// Given
 		final Long artikelId = ARTIKEL_ID_NICHTVORHANDEN;
@@ -146,7 +165,88 @@ public class ArtikelResourceTest extends AbstractResourceTest {
 
     	// Then
     	assertThat(response.getStatusCode(), is(HTTP_NOT_FOUND));
-		LOGGER.debugf("ENDE");
+		LOGGER.debugf("ENDE ARTIKELSUCHE2");
+	}
+	@Test
+	public void createArtikel() {
+		LOGGER.debugf("BEGINN ARTIKEL CREATE");
+		
+		// Given
+		final String bezeichnung = NEUE_BEZEICHNUNG;
+		final String name = NEUER_NAME;
+		final Float preis = NEUER_PREIS;
+		final Long kategorie = NEUE_KATEGORIE_ID;
+		
+		
+		
+		final JsonObject jsonObject = getJsonBuilderFactory().createObjectBuilder()
+		             		          .add("beschreibung", bezeichnung)
+		             		          .add("name", name)
+		             		          .add("preis", preis)
+		             		          .add("kategorie",getJsonBuilderFactory().createObjectBuilder().add("kategorieId",kategorie))
+		             		          .build();
+
+		// When
+		final Response response = given().contentType(APPLICATION_JSON)
+				                         .body(jsonObject.toString())
+                                         .post(ARTIKEL_PATH);
+		
+		// Then
+		assertThat(response.getStatusCode(), is(HTTP_CREATED));
+		final String location = response.getHeader(LOCATION);
+		final int startPos = location.lastIndexOf('/');
+		final String idStr = location.substring(startPos + 1);
+		final Long id = Long.valueOf(idStr);
+		assertThat(id.longValue() > 0, is(true));
+
+		LOGGER.debugf("ENDE ARTIKEL CREATE");
+	}
+	
+	@Test
+	public void updateArtikel() {
+		
+		LOGGER.debugf("BEGINN UPDATE ARTIKEL");
+		
+		//Given
+		 
+		final Long artikelId = ARTIKEL_ID_UPDATE;
+		final String neue_bezeichnung = UPDATE_BEZEICHNUNG;
+		
+		// When
+		Response response = given().header(ACCEPT, APPLICATION_JSON)
+				                   .pathParameter(ARTIKEL_ID_PATH_PARAM, artikelId)
+                                   .get(ARTIKEL_ID_PATH);
+		
+		assertThat(response.getStatusCode(), is(HTTP_OK));
+		
+		JsonObject jsonObject;
+		try (final JsonReader jsonReader =
+				              getJsonReaderFactory().createReader(new StringReader(response.asString()))) {
+			 jsonObject = jsonReader.readObject();
+		}
+    	assertThat(jsonObject.getJsonNumber("artikelId").longValue(), is(artikelId.longValue()));
+    	
+    	// Aus den gelesenen JSON-Werten ein neues JSON-Objekt mit neuem Nachnamen bauen
+    	final JsonObjectBuilder job = getJsonBuilderFactory().createObjectBuilder();
+    	final Set<String> keys = jsonObject.keySet();
+    	for (String k : keys) {
+    		if ("beschreibung".equals(k)) {
+    			job.add("beschreibung", neue_bezeichnung);
+    		}
+    		else {
+    			job.add(k, jsonObject.get(k));
+    		}
+    	}
+    	jsonObject = job.build();
+    	
+		response = given().contentType(APPLICATION_JSON)
+				          .body(jsonObject.toString())
+                          //.auth()
+                          //.basic(username, password)
+                          .put(ARTIKEL_PATH);
+		
+		// Then
+		assertThat(response.getStatusCode(), is(HTTP_NO_CONTENT));
 	}
 	
 	
