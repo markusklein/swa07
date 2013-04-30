06555554faff266ba4926731cd5bc1969c1d21f7
thomas.beuz <beth1015(at)hs-karlsruhe.de>
2013-04-30 18:55:15 +0200

    kunde_rolle.sql

---
+++ shop/src/main/resources/sql/21_kunde_rolle.sql
@@ -0,0 +1,23 @@
+-- ===============================================================================
+-- Jede SQL-Anweisung muss in genau 1 Zeile
+-- Kommentare durch -- am Zeilenanfang
+-- ===============================================================================
+
+
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (600,0);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (600,1);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (600,2);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (600,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (601,0);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (601,1);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (601,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (602,1);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (602,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (603,1);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (603,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (604,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (605,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (606,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (607,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (608,3);
+INSERT INTO kunde_rolle (kunde_fk, rolle_fk) VALUES (609,3);
